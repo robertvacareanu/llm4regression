326b13a15ce30b9b35d8ebba556be0accde3f025
Robert Vacareanu <robert.vacareanu@gmail.com>
2024-04-08 22:27:06 -0700

    Comments

---
+++ src/regressors/__init__.py
@@ -0,0 +1,5 @@
+"""
+This module contains implementations for regressors. For example:
+    - `local_llm_regressor` -> The regressor is a local Huggingface model.
+    - `openrouter_llm_regressor` -> The regressor is an OpenRouter model
+"""
